23ea58f802227f9aef834813cfff679ed59effdb
Chris <hanmir91@gmail.com>
2024-12-21 20:47:56 +0900

    feat: Add Role

--- src/main/java/com/sparta/nbcamp7lecturespringsecurityh2/entity/Member.java
+++ src/main/java/com/sparta/nbcamp7lecturespringsecurityh2/entity/Member.java
@@ -1,6 +1,9 @@
 package com.sparta.nbcamp7lecturespringsecurityh2.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -27,9 +30,50 @@ public class Member extends BaseEntity {
   private Long id;
 
   /**
+   * 사용자 이름.
+   */
+  @Column(unique = true)
+  private String username;
+
+  /**
+   * 비밀번호.
+   */
+  @Column
+  private String password;
+
+  /**
+   * 사용자의 권한. 기본값은 {@code Role.USER}입니다.
+   */
+  @Enumerated(value = EnumType.STRING)
+  private Role role = Role.USER;
+
+  /**
    * 기본 생성자.
    */
   public Member() {
   }
 
+  /**
+   * 생성자.
+   *
+   * @param username 사용자 이름
+   * @param password 비밀번호
+   */
+  public Member(String username, String password) {
+    this.username = username;
+    this.password = password;
+  }
+
+  /**
+   * 생성자.
+   *
+   * @param username 사용자 이름
+   * @param password 비밀번호
+   * @param role     사용자의 권한
+   */
+  public Member(String username, String password, Role role) {
+    this.username = username;
+    this.password = password;
+    this.role = role;
+  }
 }
---
+++ src/main/java/com/sparta/nbcamp7lecturespringsecurityh2/entity/Role.java
@@ -0,0 +1,77 @@
+package com.sparta.nbcamp7lecturespringsecurityh2.entity;
+
+import java.util.List;
+import lombok.Getter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+/**
+ * create on 2024. 12. 21. create by IntelliJ IDEA.
+ *
+ * <p> 사용자의 권한을 나타내는 Enum 클래스. </p>
+ *
+ * @author Seokgyu Hwang (Chris)
+ * @version 1.0
+ * @since 1.0
+ */
+@Getter
+public enum Role {
+  /**
+   * guest 권한.
+   */
+  GUEST("guest"),
+
+  /**
+   * user 권한.
+   */
+  USER("user"),
+
+  /**
+   * user 권한.
+   */
+  STAFF("staff"),
+
+  /**
+   * admin 권한.
+   */
+  ADMIN("admin");
+
+  /**
+   * 권한 이름.
+   */
+  private final String name;
+
+  /**
+   * 생성자.
+   *
+   * @param name 권한 이름.
+   */
+  Role(String name) {
+    this.name = name;
+  }
+
+  /**
+   * 입력받은 값에 해당하는 {@link Role}을 찾아 리턴합니다.
+   *
+   * @param roleName 권한 이름.
+   * @return {@link Role}
+   * @throws IllegalArgumentException 입력받은 값에 해당하는 권한을 찾을 수 없는 경우
+   */
+  public static Role of(String roleName) throws IllegalArgumentException {
+    for (Role role : values()) {
+      if (role.getName().equals(roleName)) {
+        return role;
+      }
+    }
+
+    throw new IllegalArgumentException("해당하는 이름의 권한을 찾을 수 없습니다: " + roleName);
+  }
+
+  /**
+   * {@link org.springframework.security.core.userdetails.UserDetails}에 담길 권한을 리턴.
+   *
+   * @return 권한 리스트. {@code List<}{@link SimpleGrantedAuthority}{@code >}
+   */
+  public List<SimpleGrantedAuthority> getAuthorities() {
+    return List.of(new SimpleGrantedAuthority("ROLE_" + this.name()));
+  }
+}
